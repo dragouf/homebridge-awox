c9c1fb8c3278a88ae086f72bf326cd403e915193
David LAURENT <da.laurent@laposte.net>
2017-10-07 12:27:30 +0200

    add mqtt support to publish lamp state and persist data

--- README.md
+++ README.md
@@ -5,10 +5,24 @@ control awox smartlight with homebridge
 ### config :
 
 ```
-{
+  {
      "accessory": "AwoxSmartLight",
      "name": "My Lamp",
      "lampMac": "d03972b84926"
+    },
+
+```
+
+with mqtt state
+```
+  {
+      "accessory": "AwoxSmartLight",
+      "name": "My Lamp",
+      "lampMac": "d03972b84926",
+      "mqttServer": ""mqtt://192.168.1.12",
+      "mqttTopicRoot": "/awox",
+      "mqttUsername": "",
+      "mqttPassword": ""
     },
 
 ```
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Accessory, Service, Characteristic, UUIDGen;
 var AwoxSmartLight = require('awox-smartlight');
 var Accessory, Service, Characteristic, UUIDGen;
+var mqtt = require('mqtt')
 
 module.exports = function(homebridge) {
   console.log("homebridge API version: " + homebridge.version);
@@ -24,7 +25,10 @@ function AwoxAccessory(log, config) {
   this.name = config["name"];
   this.bulbName = config["bulbName"] || config["name"];
   this.lampMac = config["lampMac"];
+  this.mqttServer = config["mqttServer"];
+  this.mqttTopic = config["mqttTopicRoot"];
   this.service = new Service.Lightbulb(this.name);
+  this.hasMqtt = !!config["mqttServer"];
 
   this.on = false;
   this.brightness = 100;
@@ -32,6 +36,26 @@ function AwoxAccessory(log, config) {
   this.saturation = 0;
 
   this.lamp = new AwoxSmartLight(this.lampMac, this.log);
+
+  this.client_Id = 'awox_' + Math.random().toString(16).substr(2, 8);
+  this.options = {
+		keepalive: 10,
+		clientId: this.client_Id,
+		protocolId: 'MQTT',
+		protocolVersion: 4,
+		clean: true,
+		reconnectPeriod: 1000,
+		connectTimeout: 30 * 1000,
+		will: {
+			topic: 'WillMsg',
+			payload: 'Connection Closed abnormally..!',
+			qos: 0,
+			retain: false
+		},
+		username: config["mqttUsername"],
+		password: config["mqttPassword"],
+		rejectUnauthorized: false
+	};
 
   this.service
     .getCharacteristic(Characteristic.On)
@@ -53,8 +77,15 @@ function AwoxAccessory(log, config) {
 }
 
 AwoxAccessory.prototype.getPower = function(callback) {
-  this.log("Power state for the '%s' is %s", this.name, this.on);
-  callback(null, this.on);
+    if(this.hasMqtt) {
+      this.readMqtt("power", this.on, (result) => {
+        this.log("(mqtt) Power state for the '%s' is %s", this.name, result);
+        callback(null, result === 'true');
+      });
+    } else {
+      this.log("Power state for the '%s' is %s", this.name, this.on);
+      callback(null, this.on);
+    }
 }
 
 AwoxAccessory.prototype.setPower = function(powerOn, callback) {
@@ -69,16 +100,27 @@ AwoxAccessory.prototype.setPower = function(powerOn, callback) {
 
   this.on = !powerOn;
 
+  this.writeMqtt("power", powerOn);
+
   callback();
 }
 
 AwoxAccessory.prototype.getBrightness = function(callback) {
+  if(this.hasMqtt) {
+    this.readMqtt("brightness", this.brightness, (result) => {
+      this.log("(mqtt) brightness for the '%s' is %s", this.name, result);
+      callback(null, parseFloat(result));
+    });
+  } else {
+    this.log("brightness for the '%s' is %s", this.name, this.brightness);
     callback(null, this.brightness);
+  }
 }
 
 AwoxAccessory.prototype.setBrightness = function(brightness, callback, context) {
 	if(context !== 'fromSetValue') {
     this.log("set brightness to:" + brightness);
+    this.writeMqtt("brightness", brightness);
 		this.brightness = brightness;
     this.lamp.lightBrightness(brightness / 100);
 	}
@@ -86,11 +128,20 @@ AwoxAccessory.prototype.setBrightness = function(brightness, callback, context) 
 }
 
 AwoxAccessory.prototype.getHue = function(callback) {
+  if(this.hasMqtt) {
+    this.readMqtt("hue", this.hue, (result) => {
+      this.log("(mqtt) hue for the '%s' is %s", this.name, result);
+      callback(null, parseFloat(result));
+    });
+  } else {
+    this.log("hue for the '%s' is %s", this.name, this.hue);
     callback(null, this.hue);
+  }
 }
 
 AwoxAccessory.prototype.setHue = function(hue, callback, context) {
 	if(context !== 'fromSetValue') {
+    this.writeMqtt("hue", hue);
 		this.hue = hue;
     var rgb = this._hsvToRgb(hue, this.saturation, this.brightness);
     //var r = this._decToHex(rgb.r);
@@ -103,17 +154,69 @@ AwoxAccessory.prototype.setHue = function(hue, callback, context) {
 }
 
 AwoxAccessory.prototype.getSaturation = function(callback) {
+  if(this.hasMqtt) {
+    this.readMqtt("saturation", this.saturation, (result) => {
+      this.log("saturation for the '%s' is %s", this.name, result);
+      callback(null, parseFloat(result));
+    });
+  } else {
+    this.log("saturation for the '%s' is %s", this.name, this.saturation);
     callback(null, this.saturation);
+  }
 }
 
 AwoxAccessory.prototype.setSaturation = function(saturation, callback, context) {
 	if(context !== 'fromSetValue') {
+    this.writeMqtt("saturation", saturation);
     this.log("set saturation to:" + saturation);
 		this.saturation = saturation;
     //this.lamp.lightWhiteReset();
     this.lamp.lightWhite(saturation / 100);
 	}
 	callback();
+}
+
+AwoxAccessory.prototype.writeMqtt = function(key, value)
+{
+    var logger = this.log;
+    var topic = this.mqttTopic + "/" + this.lampMac + "/" + key;
+    var client  = mqtt.connect(this.mqttServer, this.options);
+
+    logger("publishing to: '" + topic + "' value: " + value);
+
+    client.on('connect', () => {
+      client.publish(topic, value.toString(), {
+        retain: true
+      }, () => client.end());
+    });
+}
+
+AwoxAccessory.prototype.readMqtt = function(key, defaultValue, callback)
+{
+    var logger = this.log;
+
+    var requestTopic = this.mqttTopic + "/" + this.lampMac + "/" + key;
+    var client  = mqtt.connect(this.mqttServer, this.options);
+
+    client.on('connect', () => {
+      client.subscribe(requestTopic);
+    });
+
+    var timeout = setTimeout(() => {
+      logger("read mqtt timeout for " + key);
+      client.end();
+      callback(defaultValue);
+    }, 2000);
+
+    var count = 0;
+    client.on('message', (topic, message) => {
+      if(topic === requestTopic && count == 0) {
+        count++;
+        clearTimeout(timeout);
+        callback(message.toString());
+        client.end();
+      }
+    });
 }
 
 AwoxAccessory.prototype._hsvToRgb = function(h, s, v) {
